49389d2e329099c0eb7b2e45d1646108e11bf54b
Sergey Cherepanov <sergey@cherepanov.org.ua>
2016-09-13 20:50:42 +0400

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 if [[ ! -z ${USER_PASS} ]]; then
-  echo "web:"${USER_PASS} | chpasswd
+  USER_NAME=${USER_NAME-web}
+  
+  if [[ "www-data" = ${USER_NAME} ]]; then
+    chsh -s /bin/bash www-data
+  else:
+    adduser --uid 1000 --quiet --disabled-password -shell /bin/bash --home /home/web --gecos "User" ${USER_NAME} 
+  fi
+  
+  echo ${USER_NAME}":"${USER_PASS} | chpasswd
 fi
 
 if [[ ! -z ${ROOT_PASS} ]]; then
+  sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
   echo "root:"${ROOT_PASS} | chpasswd
 fi
 
